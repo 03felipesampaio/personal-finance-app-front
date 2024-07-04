472bed6a5915b526d9877103a69a0df17e72e5ca
Felipe Sampaio <03.felipesampaio@gmail.com>
2024-07-04 11:20:46 -0300

    Convert date in table

--- src/components/transactionsView/TransactionsTable.vue
+++ src/components/transactionsView/TransactionsTable.vue
@@ -3,13 +3,6 @@
     <table id="table-transactions">
       <thead>
         <tr>
-          <!-- <th>File ID</th> -->
-          <!-- <th>Date</th>
-          <th>Description</th>
-          <th>Place</th>
-          <th>Category</th>
-          <th>Value</th> -->
-
           <th v-for="col in tableColumns" :key="col">
             {{ col }}
           </th>
@@ -23,15 +16,8 @@
         >
           <!-- <td>{{ trn.sourceId }}</td> -->
           <td v-for="col in tableColumns" :key="trn.id + '_' + col">
-            {{ col !== 'date' ? trn[col] : false }}
-            <!-- {{ trn[col] }} -->
-            <!-- {{ col!=='date'? trn[col]: trn[col].format('DD/MM/YY')) }} -->
+            {{ col !== 'date' ? trn[col] : trn[col].format('DD/MM/YY') }}
           </td>
-          <!-- <td>{{ trn.date }}</td>
-          <td>{{ trn.description }}</td>s
-          <td>{{ trn.place }}</td>
-          <td>{{ trn.category }}</td>
-          <td>{{ trn.value }}</td> -->
         </tr>
       </tbody>
     </table>
